5474152f6d9f5410660574b75265286ec0c26f64
gaetanof <franciscogaetano1@gmail.com>
2024-03-14 13:36:39 -0300

    start react proyect and integrate frontegg

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-y7i7twzeyimf.frontegg.com',
+  clientId: '0970e534-33c1-4fb5-bd2a-21a76a1dd26f',
+};
+
+const authOptions = {
+  keepSessionAlive: true, // Uncomment this in order to maintain the session alive
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <FronteggProvider
+    contextOptions={contextOptions}
+    hostedLoginBox={true}
+    authOptions={authOptions}
+  >
+    <App />
+  </FronteggProvider>
+);
